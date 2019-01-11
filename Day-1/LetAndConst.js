64a2ce31f4d730562ee5b2d55e9655a63e3da4f8
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2019-01-10 22:14:05 -0800

    Create LetAndConst.js

---
+++ Day-1/LetAndConst.js
@@ -0,0 +1,8 @@
+function main() {
+
+    let r = readLine()
+    // Print the area of the circle:
+    const PI = Math.PI
+    // Print the perimeter of the circle:
+    console.log(PI * r * r);
+    console.log(PI * 2 * r);
