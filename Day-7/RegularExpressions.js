bbe592f0554fbb1e442c082182154eea43752b1e
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2019-01-11 12:47:14 -0800

    Create RegularExpressions.js
    add Regular expression example

---
+++ Day-7/RegularExpressions.js
@@ -0,0 +1,8 @@
+function regexVar(s) {
+let re = /^([aeiou]).*\1$/;
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    return re;
+}
