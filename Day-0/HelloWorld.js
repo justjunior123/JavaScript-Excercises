f8bb4b330a63b5ebe7958c3be6046d2854362374
Juan Soto <38800554+justjunior89@users.noreply.github.com>
2019-01-10 21:55:32 -0800

    Create HelloWorld.js
    Add world famous script

---
+++ Day-0/HelloWorld.js
@@ -0,0 +1,10 @@
+/*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable) 
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
